00858412951a80c013c336aae1cf08b6d1df90ee
OnyxRader <roundythomas@gmail.com>
2024-02-08 23:02:51 -0700

    Beginning Commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    String PlayerName = "NerdyNameHere"
+    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter username");
+
+    String userName = myObj.nextLine();  // Read user input
+    System.out.println("Username is: " + userName);  // Output user input
+}
